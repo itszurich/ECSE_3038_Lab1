e50b33104676a6d8e0261d262eeac564de97b1d6
Zurich Hentley <zurichentley@gmail.com>
2023-01-27 16:05:15 -0500

    hello lab questions

--- app.py
+++ app.py
@@ -5,8 +5,6 @@
 
 
 
-
-
 def parallel(resistors):
     size=len(resistors)
     if (size>=2):
@@ -28,3 +26,31 @@ parallel([330,2200,1000])
 
 		
 
+def potential_divider(voltage,r):
+    sum=0
+    for x in r:
+        sum += x
+    for x in r:
+        div=((x*voltage) / sum)
+        print('%.2f'%div,"v")
+resistors = [1010,1933,356]
+potential_divider(6,resistors[:])
+
+ 
+def temperature_check(temp,tempchar):
+	if (tempchar=="c"|tempchar =="C"):
+		if (temp<34):
+			print("youre hypothermic")
+		elif(temp>=36 & temp<38):
+			print ("youre normal")
+		else:
+            print ("youre hyperthermic")
+	if(tempchar=='f'| tempchar=='F'):
+		if (temp<95):
+			print("youre hypothermic")
+		elif (temp>=96 & temp<100):
+			print("youre normal")
+		else:
+			print("youre hyperthermic")
+
+temperature_check(32,'F')
